c1be3325dc8212b16f785ffdf432f8e66ea9bb6d
Term-inator <1121745627@qq.com>
2021-09-17 20:37:25 +0800

    组件

--- src/main/resources/static/js/mosiv.js
+++ src/main/resources/static/js/mosiv.js
@@ -73,15 +73,33 @@ class StartState extends Component {
         ctx.closePath();
         ctx.restore();
     }
+
+    contain(x, y) {
+        let distance_square = (x - this.x)*(x - this.x) + (y - this.y)*(y - this.y)
+        let r_square = this.r*this.r
+        return distance_square <= r_square
+    }
 }
 
 /**
  * 状态图的中止状态类
  */
 class EndState extends Component {
-    constructor() {
+    constructor(r, x, y) {
         super();
         this.type = 2;
+        this.r = r;
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
     }
 }
 
@@ -89,9 +107,33 @@ class EndState extends Component {
  * 状态图的普通状态类
  */
 class State extends Component {
-    constructor() {
+    constructor(x, y, width, height) {
         super();
         this.type = 3;
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+
+    draw(ctx) {
+        let r = 15
+        let point_a = {x: this.x + r, y: this.y}
+        let point_b = {x: this.x + this.width, y: this.y}
+        let point_c = {x: this.x + this.width, y: this.y + this.height}
+        let point_d = {x: this.x, y: this.y + this.height}
+        let point_e = {x: this.x, y: this.y}
+        ctx.save();
+        ctx.beginPath();
+        ctx.moveTo(point_a.x, point_a.y);
+        ctx.arcTo(point_b.x, point_b.y, point_c.x, point_c.y, r);
+        ctx.arcTo(point_c.x, point_c.y, point_d.x, point_d.y, r);
+        ctx.arcTo(point_d.x, point_d.y, point_e.x, point_e.y, r);
+        ctx.arcTo(point_e.x, point_e.y, point_a.x, point_a.y, r)
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
     }
 }
 
