986fde9a8220c472b9126b04759be6b464d59154
zhxdavid <ZHXDAVID@126.com>
2022-01-05 15:05:34 +0800

    增加依赖和测试类

--- src/test/java/cn/ecnu/mosiv/MosivApplicationTests.java
+++ src/test/java/cn/ecnu/mosiv/MosivApplicationTests.java
@@ -1,13 +1,33 @@
 package cn.ecnu.mosiv;
 
+import cn.ecnu.mosiv.Pojo.Project;
+import cn.ecnu.mosiv.Pojo.User;
+import cn.ecnu.mosiv.Pojo.nta;
+import cn.ecnu.mosiv.util.JsonUtils;
+import cn.ecnu.mosiv.util.XmlUtil;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import org.json.XML;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootTest
 class MosivApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void XmlParser() throws IOException {
+        InputStream in = JsonUtils.class.getResourceAsStream("/test-dom.xml");
+        System.out.println(in);
+        String xml = IOUtils.toString(in);
+        JSONObject xmlJSONObj = XML.toJSONObject(xml);
+        System.out.println(xmlJSONObj.toString());
     }
 
 }
